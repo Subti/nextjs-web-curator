d85615936b7d2167ba06aa665024761d2a816d96
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-07 16:36:07 -0700

    Add button and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button />
+      <main className="flex flex-col items-center justify-center">
+        <Header />
+        <Button />
+      </main>
     </>
   );
 }
