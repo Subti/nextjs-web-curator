709b9c5e7b768db207ad76060a6548c19b75ebf8
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-07 16:36:17 -0700

    Resize button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import React from "react";
 
 const Button = () => {
-  return <button className="border-2 rounded-full p-2">Submit</button>;
+  const onClick = () => {
+    alert("Hello World!");
+  };
+  return (
+    <button
+      className="text-xl rounded-full px-7 py-3 bg-gradient-to-r from-[#2298dc] to-[#7a64a7]"
+      onClick={onClick}
+    >
+      Submit
+    </button>
+  );
 };
 
 export default Button;
