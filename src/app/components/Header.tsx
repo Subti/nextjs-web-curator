49ee44cb92f2a51ccd7855642e389f1eca6a51f8
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-07 16:35:44 -0700

    Create header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text-4xl flex relative w-4/5 h-32 bg-[#2298dc] justify-center items-center">
+      Capture recording from software defined radio
+    </div>
+  );
+};
+
+export default Header;
