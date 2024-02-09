57765232d801d64a571f0c33fd1da898ccb59ff4
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-09 15:56:34 -0700

    Add change handler, classes

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,17 +1,26 @@
+"use client";
+
 import React from "react";
+import { useState } from "react";
 
 const Form = (props: any) => {
+  const [value, setValue] = useState(props.value);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
   return (
     <>
-      <label>{props.label}:</label>
+      <label className="font-bold text-[18px] mb-2">{props.label}:</label>
       <input
-        className="w-1/2 mb-2"
+        className="mb-2 py-2 mb-8 custom-border-input"
         type="text"
         id={props.id}
         name={props.id}
-        value={props.value}
+        value={value}
+        onChange={handleChange}
       />
-      <hr />
     </>
   );
 };
