8373d4c3695eb66759a04a50c2535853441ecb26
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-09 15:58:37 -0700

    Refactor code, remove capture and metadata modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,3 @@
-// import Button from "./components/Button";
-// import Column from "./components/Column";
-// import Header from "./components/Header";
-// import Row from "./components/Row";
-// import Form from "./components/Form";
-
-// export default function Home() {
-//   return (
-//     <>
-//       <main className="flex flex-col items-center justify-center min-h-screen">
-//         <Header />
-//         <div className="flex flex-grow bg-white w-1/2">
-//           <Column title="Capture Settings">
-//             <Form
-//               label="IP Address"
-//               id="ip_address"
-//               name="ip_address"
-//               value="192.168.40.2"
-//             />
-//           </Column>
-//           <Column title="Metadata" />
-//         </div>
-//         <Button />
-//       </main>
-//     </>
-//   );
-// }
-
-// Home.tsx
 import React from "react";
 import Button from "./components/Button";
 import Column from "./components/Column";
@@ -35,25 +6,6 @@ import captureSettingsForms from "./modules/captureSettingsForms";
 import metadataForms from "./modules/metadataForms";
 
 export default function Home() {
-  // const captureSettingsForms = [
-  //   {
-  //     label: "IP Address",
-  //     id: "ip_address",
-  //     name: "ip_address",
-  //     value: "192.168.40.2"
-  //   }
-  //   // Add more form data objects as needed
-  // ];
-
-  // const metadataForms = [
-  //   {
-  //     label: "Protocol",
-  //     id: "protocol",
-  //     name: "protocol",
-  //     value: "Wifi-BGN"
-  //   }
-  // ];
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Header />
@@ -61,9 +13,7 @@ export default function Home() {
         <Column title="Capture Settings" forms={captureSettingsForms} />
         <Column title="Metadata" forms={metadataForms} />
       </div>
-      <div className="flex justify-end">
-        <Button />
-      </div>
+      <Button />
     </div>
   );
 }
