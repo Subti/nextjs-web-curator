60bf1ce9ccba535dd6845e0586e1e33aee1c5e48
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-16 12:20:45 -0700

    Change Form props

--- src/app/components/Column.tsx
+++ src/app/components/Column.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import Form from "./Form";
+import Button from "./Button";
 
 interface FormData {
   label: string;
@@ -23,22 +24,24 @@ const Column: React.FC<ColumnProps> = ({ title, forms }) => {
     forms.reduce((values, form) => ({ ...values, [form.id]: form.value }), {})
   );
 
-  const handleFormChange = (id: string, value: string) => {
-    setFormValues(values => ({ ...values, [id]: value }));
-  };
+  // const handleFormChange = (id: string, value: string) => {
+  //   setFormValues(values => ({ ...values, [id]: value }));
+  // };
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
     const processedFormValues: { [key: string]: string | number } = {
-      ...formValues,
+      ...formValues
     };
 
     if (formValues.num_samples) {
       processedFormValues.num_samples = parseInt(formValues.num_samples);
     }
     if (formValues.center_frequency) {
-      processedFormValues.center_frequency = parseFloat(formValues.center_frequency);
+      processedFormValues.center_frequency = parseFloat(
+        formValues.center_frequency
+      );
     }
     if (formValues.gain) {
       processedFormValues.gain = parseFloat(formValues.gain);
@@ -54,24 +57,24 @@ const Column: React.FC<ColumnProps> = ({ title, forms }) => {
     }
 
     for (let pair of formData.entries()) {
-      console.log(pair[0] + ', ' + pair[1]);
+      console.log(pair[0] + ", " + pair[1]);
     }
 
     try {
-      const response = await fetch('http://localhost:8000/', {
-        method: 'POST',
-        body: formData,
+      const response = await fetch("http://localhost:8000/", {
+        method: "POST",
+        body: formData
       });
 
       if (!response.ok) {
-        console.error('HTTP error', response.status);
+        console.error("HTTP error", response.status);
       } else {
         const data = await response.json();
         console.log(data);
         setImageUrl(data.image_url._url); // Updated this line
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
@@ -82,17 +85,16 @@ const Column: React.FC<ColumnProps> = ({ title, forms }) => {
   }, [imageUrl, router]);
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col flex-grow border-1 rounded-md shadow-xl w-1/2 py-8 px-3 mb-3">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col flex-grow border-1 rounded-md shadow-xl w-1/2 py-8 px-3 mb-3"
+    >
       <h2 className="text-center text-3xl text-[#2298dc] mb-7">{title}</h2>
       {forms.map((formData, index) => (
-        <Form
-          key={index}
-          {...formData}
-          value={formValues[formData.id]}
-          onChange={(value) => handleFormChange(formData.id, value)}
-        />
+        <Form key={index} {...formData} />
+        // onChange={(value) => handleFormChange(formData.id, value)}
       ))}
-      <button type="submit">Submit</button>
+      <Button />
     </form>
   );
 };
