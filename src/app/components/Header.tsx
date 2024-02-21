82a7562ddc3a646d042a61c14d660cb36bea7e44
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-21 15:11:31 -0700

    Add props to header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,12 +6,10 @@ import React from "react";
  * Represents the header section of the application.
  * Displays a title centered within a colored background.
  */
-const Header = () => {
+export default function Header(props: any) {
   return (
     <div className="text-4xl text-white flex relative w-4/5 h-32 bg-[#2298dc] justify-center items-center mb-4">
-      Capture recording from software defined radio
+      {props.title}
     </div>
   );
-};
-
-export default Header; // Exporting Header component
+}
