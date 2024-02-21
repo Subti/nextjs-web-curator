2e04e5aa03de511cc2c1fdb12860f85dfb1b2b3c
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-21 16:46:05 -0700

    Begin styling component

--- src/app/components/ReviewSettings.tsx
+++ src/app/components/ReviewSettings.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function ReviewSetting() {
-  return <div>ReviewSetting</div>;
+  return (
+    <div className="flex flex-col border-1 rounded-md shadow-xl custom-height-300 custom-width-300 py-8 px-3 mb-3"></div>
+  );
 }
