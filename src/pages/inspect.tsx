7dd7170f93cf1d0007a017188431c5ddaabbe798
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-21 16:45:32 -0700

    Import ReviewSettings and Button components

--- src/pages/inspect.tsx
+++ src/pages/inspect.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "@/app/components/Header"; // Importing Header component
 import ReviewSettings from "@/app/components/ReviewSettings";
+import Button from "@/app/components/Button";
 
 export default function Inspect(props: any) {
   return (
     <div className="flex flex-col items-center justify-center">
       <Header title="Inspect Recording" />
-      <ReviewSettings />
+      <div className="flex justify-around w-3/4">
+        <ReviewSettings />
+        <ReviewSettings />
+        <ReviewSettings />
+      </div>
+      <Button text="Discard and Capture New" />
     </div>
   );
 }
