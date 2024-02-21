65b567e93145d950bb045ce959d5ae4b0f23960d
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-21 16:07:13 -0700

    Import ReviewSettings component

--- src/pages/inspect.tsx
+++ src/pages/inspect.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "../app/components/Header"; // Importing Header component
+import Header from "@/app/components/Header"; // Importing Header component
+import ReviewSettings from "@/app/components/ReviewSettings";
 
 export default function Inspect(props: any) {
   return (
     <div className="flex flex-col items-center justify-center">
       <Header title="Inspect Recording" />
+      <ReviewSettings />
     </div>
   );
 }
