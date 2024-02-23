6f8b4af01e23a18ff296b41c3e2adab8482e7895
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-23 11:41:26 -0700

    Add HeaderProps interface and change width

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,10 +6,15 @@ import React from "react";
  * Represents the header section of the application.
  * Displays a title centered within a colored background.
  */
-export default function Header(props: any) {
+
+interface HeaderProps {
+  title: string;
+}
+
+export default function Header({ title }: HeaderProps) {
   return (
-    <div className="text-4xl text-white flex relative w-4/5 h-32 bg-[#2298dc] justify-center items-center mb-4">
-      {props.title}
+    <div className="text-4xl text-white flex relative w-full h-32 bg-[#2298dc] justify-center items-center mb-4">
+      {title}
     </div>
   );
 }
