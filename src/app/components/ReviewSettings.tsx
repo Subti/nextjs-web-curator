1da7babcba02f341a6c5c4c0ee4597e8b810dbb3
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-22 17:00:31 -0700

    Add heading to component

--- src/app/components/ReviewSettings.tsx
+++ src/app/components/ReviewSettings.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-export default function ReviewSetting() {
+export default function ReviewSetting(props: any) {
   return (
-    <div className="flex flex-col border-1 rounded-md shadow-xl custom-height-300 custom-width-300 py-8 px-3 mb-3"></div>
+    <div className="flex flex-col border-1 rounded-md shadow-xl custom-height-325 custom-width-325 py-12 px-3 mb-3">
+      <h2 className="text-center text-3xl text-[#2298dc] mb-7">
+        {props.title}
+      </h2>
+    </div>
   );
 }
