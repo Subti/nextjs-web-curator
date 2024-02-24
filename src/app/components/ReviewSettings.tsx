15aa84a2ea79fae64056a739de0257564071099c
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-24 09:53:44 -0700

    Added FormData interface and render conditions for data

--- src/app/components/ReviewSettings.tsx
+++ src/app/components/ReviewSettings.tsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import Button from "./Button";
 
+interface FormDataItem {
+  id: any;
+  name: any;
+  label: any;
+  value: any;
+}
+
 interface ReviewSettingProps {
   title: string;
   renderFormAndButton: boolean;
+  formData: FormDataItem[] | null;
 }
 
 export default function ReviewSetting({
   title,
-  renderFormAndButton
+  renderFormAndButton,
+  formData
 }: ReviewSettingProps) {
   const [value, setValue] = useState("");
 
@@ -20,12 +29,20 @@ export default function ReviewSetting({
   return (
     <div className="flex flex-col border-1 rounded-md shadow-xl custom-height-400 custom-width-400 py-8 px-3 mb-3">
       <h2 className="text-center text-3xl text-[#2298dc] mb-7">{title}</h2>
+      {formData && (
+        <div>
+          {formData.map((item) => (
+            <div key={item.id}>
+              <span className="font-bold">{item.label}:</span> {item.value}
+            </div>
+          ))}
+        </div>
+      )}
       {renderFormAndButton && (
         <div className="flex flex-col justify-center items-center">
           <p>Enter the cut points as space seperated values</p>
           <br />
           <p>For example: 10 18 25 30...</p>
-          {/* Your form goes here */}
           <form>
             <input
               className="mb-2 py-2 mb-8 border border-black border-solid rounded-md w-60 mt-5"
@@ -36,7 +53,6 @@ export default function ReviewSetting({
               onChange={handleChange} // Binding handleChange function to onChange event
             />
           </form>
-          {/* Your button goes here */}
           <Button type="submit" text="Save and Prune" />
         </div>
       )}
