98c405aac96d7d1957c25580c9fec4ed1bff0d55
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-25 16:12:05 -0700

    Refactor DataProvider

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -5,26 +5,48 @@ interface Props {
   children: ReactNode;
 }
 
+interface CaptureSettingsData {
+  image_url: { _url: string };
+  rec_args: {
+    /* rec_args properties */
+  };
+  capture_args: {
+    /* capture_args properties */
+  };
+  metadata: {
+    /* metadata properties */
+  };
+}
+
 interface ContextType {
-  fetchedData1: null | any; // Change 'any' to the type of your data if known
-  setFetchedData1: React.Dispatch<React.SetStateAction<null | any>>;
-  fetchedData2: null | any; // Change 'any' to the type of your data if known
-  setFetchedData2: React.Dispatch<React.SetStateAction<null | any>>;
+  captureSettingsData: CaptureSettingsData | any; // Change 'any' to the type of your data if known
+  setCaptureSettingsData: React.Dispatch<
+    React.SetStateAction<CaptureSettingsData | any>
+  >;
 }
 
 const DataContext = createContext<ContextType | null>(null);
 
 export const DataProvider = ({ children }: Props) => {
-  const [fetchedData1, setFetchedData1] = useState(null);
-  const [fetchedData2, setFetchedData2] = useState(null);
+  const [captureSettingsData, setCaptureSettingsData] = useState<
+    CaptureSettingsData | any
+  >(null);
+
+  const contextValue: ContextType = {
+    captureSettingsData,
+    setCaptureSettingsData
+  };
 
   return (
-    <DataContext.Provider
-      value={{ fetchedData1, setFetchedData1, fetchedData2, setFetchedData2 }}
-    >
-      {children}
-    </DataContext.Provider>
+    <DataContext.Provider value={contextValue}>{children}</DataContext.Provider>
   );
 };
 
-export const useData = () => useContext(DataContext);
+// Custom hook to access context
+export const useData = () => {
+  const context = useContext(DataContext);
+  if (!context) {
+    throw new Error("useData must be used within a DataProvider");
+  }
+  return context;
+};
