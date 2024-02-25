5d2ac6d09484db04f28c1ca319886eadd45810b9
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-24 17:33:58 -0700

    Change file and component name

---
+++ src/app/modules/metadataData.tsx
@@ -0,0 +1,40 @@
+const metadataData = [
+  {
+    label: "Protocol",
+    id: "protocol",
+    name: "protocol",
+    value: "Wifi-BGN"
+  },
+  {
+    label: "Use Case",
+    id: "use_case",
+    name: "use_case",
+    value: "ambient"
+  },
+  {
+    label: "Testbed",
+    id: "testbed",
+    name: "testbed",
+    value: "Portable-Blade"
+  },
+  {
+    label: "Transmitters",
+    id: "transmitters",
+    name: "transmitters",
+    value: "Ambient"
+  },
+  {
+    label: "Project Name",
+    id: "project_name",
+    name: "project_name",
+    value: "Collision2023"
+  },
+  {
+    label: "SDR - Options: ['synth']",
+    id: "sdr",
+    name: "sdr",
+    value: "synth"
+  }
+];
+
+export default metadataData;
---
+++ src/app/modules/recordingSummaryData.tsx
@@ -0,0 +1,22 @@
+const recordingSummaryData = [
+  {
+    label: "Plot Time",
+    id: "plot_time",
+    name: "plot_time",
+    value: "full"
+  },
+  {
+    label: "Save Plot",
+    id: "save_plot",
+    name: "save_plot",
+    value: "True"
+  },
+  {
+    label: "File Name",
+    id: "file_name",
+    name: "file_name",
+    value: "recordings/iq24440MHz164450.npy"
+  }
+];
+
+export default recordingSummaryData;
