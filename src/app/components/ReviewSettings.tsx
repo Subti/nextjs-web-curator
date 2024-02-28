dc1b3487a79daffc2aac05969e1ac75b68fb85d3
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-02-28 15:35:36 -0700

    Tranformed string data to objects

--- src/app/components/ReviewSettings.tsx
+++ src/app/components/ReviewSettings.tsx
@@ -1,17 +1,12 @@
-import React, { useState } from "react";
-import Button from "./Button";
+"use client";
 
-interface FormDataItem {
-  id: any;
-  name: any;
-  label: any;
-  value: any;
-}
+import React, { useEffect, useState } from "react";
+import Button from "./Button";
 
 interface ReviewSettingProps {
   title: string;
   renderFormAndButton: boolean;
-  formData: FormDataItem[] | null;
+  formData: string | null;
 }
 
 export default function ReviewSetting({
@@ -20,20 +15,46 @@ export default function ReviewSetting({
   formData
 }: ReviewSettingProps) {
   const [value, setValue] = useState("");
+  const [data, setData] = useState({});
 
-  // Event handler for input change
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
+
+  function transformKeys(obj: { [key: string]: any }): { [key: string]: any } {
+    const transformedObj: { [key: string]: any } = {};
+    for (let key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        const newKey = key
+          .split("_")
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ");
+        transformedObj[newKey] = obj[key];
+      }
+    }
+    return transformedObj;
+  }
+
+  useEffect(() => {
+    if (formData) {
+      const timeout = setTimeout(() => {
+        const formObj = JSON.parse(formData);
+        setData(transformKeys(formObj));
+        console.log("Setting Displayed");
+      }, 100);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [formData]);
 
   return (
     <div className="flex flex-col border-1 rounded-md shadow-xl custom-height-400 custom-width-400 py-8 px-3 mb-3">
       <h2 className="text-center text-3xl text-[#2298dc] mb-7">{title}</h2>
       {formData && (
         <div>
-          {formData.map((item) => (
-            <div key={item.id}>
-              <span className="font-bold">{item.label}:</span> {item.value}
+          {Object.entries(data).map(([key, value]) => (
+            <div key={key}>
+              <span className="font-bold">{key}:</span> {value as any}
             </div>
           ))}
         </div>
