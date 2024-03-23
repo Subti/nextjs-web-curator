25d1ddcb99f9752167e783d004ba1ce14cd872d7
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-22 20:46:30 -0600

    Change button text size

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,9 @@
+"use client";
 import React from "react";
 import Button from "./Button";
 import NavLinks from "../modules/constants";
 
-interface HeaderProps {
-  title: string;
-}
-
-export default function Header({ title }: HeaderProps) {
+export default function Header() {
   const displayMessage = () => {
     console.log(
       "Take me home, country roads... to the place, I beloooooooong..."
@@ -14,7 +11,7 @@ export default function Header({ title }: HeaderProps) {
   };
 
   return (
-    <div className="text-4xl text-white flex relative w-full h-[100px] bg-transparent border-b-2 border-white justify-between items-center pl-[8rem] pr-[8rem] mb-4">
+    <div className="fixed top-0 text-4xl text-white flex relative w-full h-[100px] bg-transparent border-b-2 border-white justify-between items-center pl-[8rem] pr-[8rem] mb-4">
       <a
         href="https://www.qoherent.ai"
         target="_blank"
@@ -25,11 +22,14 @@ export default function Header({ title }: HeaderProps) {
       </a>
       <div className="flex flex-row items-center">
         {NavLinks.map((item) => (
-          <p key={item.id} className="text-[1rem] mr-10 cursor-pointer">
+          <p
+            key={item.id}
+            className={`text-base mr-10 cursor-pointer transition-all duration-500 hover:text-[#03A9F4] hover:scale-110`}
+          >
             {item.id}
           </p>
         ))}
-        <Button text="Logout" type="submit" textSize="sm" />
+        <Button text="Logout" type="button" textSize="base" />
       </div>
     </div>
   );
