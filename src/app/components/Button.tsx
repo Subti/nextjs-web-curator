ebeea2bac734a7ab8d5c45416b90eae16dbb72d4
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-22 21:07:37 -0600

    Add onClick prop

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -14,6 +14,7 @@ interface ButtonProps {
   textSize: keyof TextSize;
   type: "submit" | "reset" | "button" | undefined;
   text: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Button = (props: ButtonProps) => {
@@ -31,6 +32,7 @@ const Button = (props: ButtonProps) => {
         textSize[props.textSize]
       } text-white rounded-lg px-7 py-3 bg-transparent border-[1px] border-white transition-colors duration-500 hover:bg-[#03A9F4] self-center`}
       type={props.type}
+      onClick={props.onClick}
     >
       {props.text}
     </button>
