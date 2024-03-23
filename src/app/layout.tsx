e2b5cbf1a546c47a6a4a26da812eb71e56966495
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-22 20:45:28 -0600

    Import and render Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google"; // Importing Inter font from Google Fonts
 import "./globals.css"; // Importing global CSS styles
+import Header from "./components/Header";
 
 // Initializing Inter font with Latin subset
 const inter = Inter({ subsets: ["latin"] });
@@ -26,7 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* Body element with Inter font styles applied */}
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
