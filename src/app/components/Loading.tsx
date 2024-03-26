e2691c304ff38cdda2b18fac06de194cdec2f33b
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-26 15:27:10 -0600

    Add dynamic text prop

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Loading = () => {
+interface LoadingProps {
+  text: string;
+}
+
+const Loading = ({ text }: LoadingProps) => {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen z-50 flex justify-center items-center bg-gray-900 bg-opacity-75">
       <div className="flex flex-col gap-10 items-center justify-center">
-        <h1 className="text-white text-[2rem]">Generating Signal</h1>
+        <h1 className="text-white text-[2rem]">{text}</h1>
         <div className="loader ease-linear rounded-full border-8 h-24 w-24"></div>
       </div>
     </div>
