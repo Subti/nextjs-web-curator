4c4f7a9e9b66e64a4a5a31241817eecc3f21d6de
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-03-25 21:12:00 -0600

    Edit spinner

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen z-50 flex justify-center items-center bg-gray-900 bg-opacity-50">
-      <div className="flex flex-col gap-5 items-center">
+    <div className="fixed top-0 left-0 w-screen h-screen z-50 flex justify-center items-center bg-gray-900 bg-opacity-75">
+      <div className="flex flex-col gap-10 items-center justify-center">
         <h1 className="text-white text-[2rem]">Generating Signal</h1>
-        <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-24 w-24"></div>
+        <div className="loader ease-linear rounded-full border-8 h-24 w-24"></div>
       </div>
     </div>
   );
